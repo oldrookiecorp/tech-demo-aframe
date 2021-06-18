7513d88a9912509b36d1b7899300aa48d9da38aa
KyeongSoo Yoo <dev.yoogomja@gmail.com>
2021-06-18 16:04:24 +0900

    aframe-state-component 추가

--- server/routes/renderer.js
+++ server/routes/renderer.js
@@ -7,7 +7,8 @@ const renderPage = (head, body, jsFilename, data) => {
 			</script>
       <script src="https://aframe.io/releases/1.2.0/aframe.js"></script>
       <script src="https://mixedreality.mozilla.org/ammo.js/builds/ammo.wasm.js"></script>
-      <script src="http://cdn.jsdelivr.net/gh/n5ro/aframe-physics-system@v4.0.1/dist/aframe-physics-system.js"></script>
+      <script src="https://cdn.jsdelivr.net/gh/n5ro/aframe-physics-system@v4.0.1/dist/aframe-physics-system.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/aframe-state-component@7.1.0/dist/aframe-state-component.min.js"></script>
 			<script src="${jsFilename}" ></script>
 		</head>
 		<body>
